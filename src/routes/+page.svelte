8619afd86bf55218d11d6e3b4e7554ec7d78f0ec
edde746 <86283021+edde746@users.noreply.github.com>
2022-08-27 19:03:59 +0200

    Landing Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,54 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import LDtag from '$lib';
+</script>
+
+<main>
+  <h1>Svelte LD+JSON</h1>
+  <p>Easily add ld+json to your Svelte project</p>
+  <span>Example:</span>
+  <pre>
+{`${'<'}script>
+  import LDtag from 'svelte-ld-json';
+</script>
+
+<LDtag
+  schema={{
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'John Doe',
+    url: 'https://example.com/',
+    sameAs: ['https://twitter.com/johndoe', 'https://facebook.com/johndoe']
+  }}
+/>`}
+</pre>
+</main>
+
+<!-- Example for page -->
+<LDtag
+  schema={{
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'John Doe',
+    url: 'https://example.com/',
+    sameAs: ['https://twitter.com/johndoe', 'https://facebook.com/johndoe'],
+  }}
+/>
+
+<style>
+  main {
+    font-family: sans-serif;
+    max-width: 50rem;
+    margin: 0 auto;
+  }
+
+  pre {
+    background-color: #f5f5f5;
+    border: 1px solid #ccc;
+    padding: 1rem;
+  }
+
+  span {
+    font-size: 0.75rem;
+    color: rgb(80, 80, 80);
+  }
+</style>
